f6d54f4ce530b1ad4d4cc43ab25676e59108606c
Zuon <lordzuon@gmail.com>
2013-05-15 20:27:05 -0400

    more documentation, including doxygen

--- daisy.cpp
+++ daisy.cpp
@@ -11,6 +11,7 @@ unsigned uabs(int x){ return x<0?-x:x; }
 
 float interpolate(float a, float b, float bness){ return a*(1-bness)+b*bness; }
 
+//map [xi, xf] to [yi, yf]
 float linear(float x, float xi, float xf, float yi, float yf){
 	return (x-xi)/(xf-xi)*(yf-yi)+yi;
 }
@@ -23,8 +24,10 @@ Motor::Motor(float maxSpeed, float maxAcceleration):
 }
 
 void Motor::step(bool positive){
+	//figure the motor's speed between now and the last step and check if it's too fast
 	if(ticksSinceLastStep!=0&&1.0f/ticksSinceLastStep>maxSpeed)
 		wentTooFast.push_back(t);
+	//figure the motor's acceleartion between now and the last last step and check if it's too much
 	if(
 		ticksBetweenPreviousSteps!=0
 		&&
@@ -33,6 +36,7 @@ void Motor::step(bool positive){
 		maxAcceleration
 	)
 		acceleratedTooFast.push_back(t);
+	//step and document
 	ticksBetweenPreviousSteps=ticksSinceLastStep;
 	ticksSinceLastStep=0;
 	positive?++x:--x;
@@ -86,11 +90,11 @@ bool Axis::tick(){
 	++ticks;
 	//update u
 	if(uM==du) nextMovement();
-	const unsigned f=frequencyMultiplier;
-	const unsigned b=FRACTIONAL_BITS;
+	const unsigned f=frequencyMultiplier;//just make stuff shorter so it's easier to see
+	const unsigned b=FRACTIONAL_BITS;//just make stuff shorter so it's easier to see
 	if(
 		uabs(int(2*dt*(uT+1)*f*f-dupfSign*(dupf*ticks*ticks>>b)-(2*dt*upif*ticks>>b)*f))
-		<               //
+		<               //this is the only difference!
 		uabs(int(2*dt*(uT+0)*f*f-dupfSign*(dupf*ticks*ticks>>b)-(2*dt*upif*ticks>>b)*f))
 	){
 		++uT;
@@ -99,7 +103,11 @@ bool Axis::tick(){
 	}
 	//update x
 	motor.tick();
-	if(uabs(int((x+1)*du-dx*uM))<uabs(int(x*du-dx*uM))){
+	if(
+		uabs(int((x+1)*du-dx*uM))
+		<         //this is the only difference!
+		uabs(int((x+0)*du-dx*uM))
+	){
 		motor.step(dxPositive);
 		++x;
 		stepped=true;
@@ -132,33 +140,41 @@ unsigned Axis::readRemainingTimings() const{ return upcomingDu.size(); }
 unsigned Axis::readU() const{ return u; }
 
 void Axis::nextMovement(){
+	//reset
 	x=0;
 	uM=0;
+	//if there are no more movements, don't move
 	if(!upcomingDx.size()){
 		dx=0;
 		du=1;
 		finished=true;
 		return;
 	}
+	//if there are more movements, get the next one
 	dx=uabs(upcomingDx[0]);
 	du=upcomingDu[0];
 	dxPositive=upcomingDx[0]>=0?true:false;
+	//and erase it from the list
 	upcomingDx.erase(upcomingDx.begin());
 	upcomingDu.erase(upcomingDu.begin());
 }
 
 void Axis::nextTiming(){
+	//prepare for next
 	uT=0;
 	ticks=0;
 	upif+=dupfSign*dupf;
+	//if there are no more timings, just do u'(t)=1
 	if(!upcomingDupf.size()){
 		dupf=0;
 		dt=1<<FRACTIONAL_BITS;
 		return;
 	}
+	//if there are more timings, get the next one
 	dupf=uabs(upcomingDupf[0]);
 	dt=upcomingDt[0];
 	dupfSign=upcomingDupf[0]>=0?1:-1;
+	//and erase it from the list
 	upcomingDupf.erase(upcomingDupf.begin());
 	upcomingDt.erase(upcomingDt.begin());
 }
@@ -170,8 +186,8 @@ void Hub::addAxis(
 	float maxSpeed, float maxAcceleration, unsigned frequencyMultiplier
 ){
 	axes.push_back(Axis(maxSpeed, maxAcceleration, frequencyMultiplier));
+	//make sure factors(frequencyMultiplier) is a subset of factors(subTicksPerTick)
 	if(subTicksPerTick%frequencyMultiplier!=0){
-		//make sure factors(frequencyMultiplier) is a subset of factors(subTicksPerTick)
 		unsigned r=subTicksPerTick;
 		for(unsigned factor=2; frequencyMultiplier!=1; factor==2?factor=3:factor+=2){
 			bool factorFound;
@@ -200,18 +216,25 @@ void Hub::setTiming(int dupf, unsigned dt){
 float Hub::subTick(){
 	unsigned previousSubTicks=subTicks;
 	bool stepHappened=false;
+	//tick axes until a step happens
 	while(!stepHappened){
 		bool allFinished=true;
+		//tick axes and note whether they stepped or finished
 		for(unsigned i=0; i<axes.size(); ++i){
 			allFinished&=axes[i].readFinished();
 			if(subTicks%(subTicksPerTick/axes[i].readFrequencyMultiplier())==0)
 				if(axes[i].tick()) stepHappened=true;
 		}
+		//if all have finished, the run is done
 		if(allFinished) return 0.0f;
+		//increment
 		++subTicks;
 	}
+	//measure how much time has passed
 	float result=1.0f*(subTicks-previousSubTicks)/subTicksPerTick;
+	//keep this from overflowing
 	subTicks%=subTicksPerTick;
+	//finish
 	return result;
 }
 
@@ -233,16 +256,19 @@ string toString(int i){
 string Computer::setScript(string script){
 	segments.clear();
 	hub=Hub();
-	//parse
+	//initialize parsing stuff
 	stringstream scriptStream(script);
 	string line;
 	int lineNumber=1;
 	bool inBody=false;
 	float maxSpeed=0.0f;
+	//parse
 	while(getline(scriptStream, line)){
+		//get a token
 		stringstream lineStream(line);
 		string token;
 		lineStream>>token;
+		//see what the token is and process the rest of the line
 		if(token=="axis"){
 			if(inBody) return
 				"Error on line "+
@@ -262,8 +288,10 @@ string Computer::setScript(string script){
 			if(error!="") return "Error on line "+toString(lineNumber)+": "+error;
 		}
 		else if(token=="maxSpeed") lineStream>>maxSpeed;
+		//if it's not something we know, it's an error
 		else
 			return "Error on line "+toString(lineNumber)+": unknown specification.";
+		//increment
 		++lineNumber;
 	}
 	//done
@@ -273,12 +301,16 @@ string Computer::setScript(string script){
 void Computer::setTiming(const vector<UpfPoint>& _upf){ upf=_upf; }
 
 void Computer::sendToHub(){
+	//send movements
 	for(unsigned i=0; i<segments.size(); ++i){
+		//Determine how much u this movement gets. Ensure no more than one step per tick is required.
 		int maxX=0;
 		for(unsigned j=0; j<segments[i].readX().size(); ++j)
 			maxX=max(maxX, abs(segments[i].readX()[j]));
+		//send
 		hub.setMovement(segments[i].readX(), maxX);
 	}
+	//send timings
 	for(unsigned i=1; i<upf.size(); ++i)
 		hub.setTiming(upf[i].upf-upf[i-1].upf, upf[i].t-upf[i-1].t);
 }
@@ -289,33 +321,44 @@ void Computer::getTheoreticalPosition(float u, unsigned i, float& x) const{
 	x=0.0f;
 	float previous=x;
 	unsigned j, maxX=0;
+	//figure out what segment u goes into
 	for(j=0; j<segments.size(); ++j){
+		//keep track of theoretical position
 		previous=x;
 		x+=segments[j].readX()[i];
+		//figure out how much u is allocated to this segment
 		maxX=0;
 		for(unsigned k=0; k<segments[j].readX().size(); ++k)
 			maxX=max(maxX, uabs(segments[j].readX()[k]));
+		//if we're beyond this segment
 		if(u>maxX){
-			u-=maxX;
-			if(j==segments.size()-1) return;
+			u-=maxX;//decrease u accordingly and carry on
+			if(j==segments.size()-1) return;//if u is not in a segment, return the final location
 		}
+		//otherwise, this is the segment
 		else break;
 	}
+	//interpolate between start and end of that segment
 	x=linear(u, 0, maxX, previous, x);
 }
 
 float Computer::getMaxSpeed(float u) const{
 	unsigned i;
+	//figure out what segment u goes into
 	for(i=0; i<segments.size(); ++i){
+		//figure out how much u is allocated to this segment
 		unsigned maxX=0;
 		for(unsigned j=0; j<segments[i].readX().size(); ++j)
 			maxX=max(maxX, uabs(segments[i].readX()[j]));
+		//if we're beyond this segment
 		if(u>maxX){
-			u-=maxX;
-			if(i==segments.size()-1) break;
+			u-=maxX;//decrease u accordingly and carry on
+			if(i==segments.size()-1) break;//if u is not in a segment, use the last segment
 		}
+		//otherwise, this is the segment
 		else break;
 	}
+	//return the max speed of the segment
 	return segments[i].readMaxSpeed();
 }
 
@@ -345,12 +388,16 @@ const vector<int>& Computer::Segment::readX() const{ return x; }
 //=====System=====//
 string System::setScript(string script){ return computer.setScript(script); }
 
+//find a point near t
+//if there isn't one, insert one
+//return the index of that point
 int insertIfNoneNear(float t, vector<UpfPoint>& upf){
+	//this shouldn't happen!
 	if(t<0) return -1;
-	//try to find a nearby point
+	//find the nearest previous point, and try to find a near point
 	int index=-1;
 	unsigned previous=0;
-	float difference=1.0f;
+	float difference=1.0f;//this defines what near is
 	for(unsigned i=0; i<upf.size(); ++i){
 		if(abs(t-upf[i].t)<difference){
 			index=i;
@@ -360,24 +407,35 @@ int insertIfNoneNear(float t, vector<UpfPoint>& upf){
 	}
 	//insert if not found
 	if(index<0){
-		unsigned upfPrevious=upf[previous].upf;
+		//we desire the upf at this place
 		unsigned upfHere;
+		//get the previous upf value
+		unsigned upfPrevious=upf[previous].upf;
+		//get the next upf value
 		if(previous+1>=upf.size()) upfHere=upfPrevious;
 		else{
 			unsigned upfNext=upf[previous+1].upf;
 			upfHere=(unsigned)interpolate(1.0f*upfPrevious, 1.0f*upfNext, 1.0f*(t-upf[previous].t)/(upf[previous+1].t-upf[previous].t));
 		}
+		//interpolate and insert
 		upf.insert(upf.begin()+previous+1, UpfPoint(upfHere, (unsigned)t));
+		//set the index accordingly
 		index=previous+1;
 	}
 	//return index of point
 	return index;
 }
 
+//given a coverage and a upf, dip upf at t if it's not covered
+//a dip consists of the following:
+//upf near t is reduced by a factor, then
+//upf[T] where t-dt<T<t is ensured to be less than the interpolation between upf[t-dt] and upf[t], and
+//upf[T] where t<T<t+dt is ensured to be less than the interpolation between upf[t] and upf[t+dt]
 void dip(vector<UpfPoint>& upf, float t, vector<float>& coverage){
 	const float dt=128.0f;//empirically chosen
 	//check if this spot has been covered already
 	for(unsigned i=0; i<coverage.size(); ++i) if(abs(t-coverage[i])<dt) return;
+	//set this spot as covered
 	coverage.push_back(t);
 	//ensure enough points to create a dip
 	int leftIndex=insertIfNoneNear(t-dt, upf);
@@ -391,43 +449,50 @@ void dip(vector<UpfPoint>& upf, float t, vector<float>& coverage){
 	//make the dip
 	float ti=t-dt;//t initial
 	float tf=t+dt;//t final
-	upf[index].upf/=2;
-	unsigned lowUpf=upf[index].upf;
-	unsigned leftHighUpf=1<<FRACTIONAL_BITS, rightHighUpf=1<<FRACTIONAL_BITS;
-	if(leftIndex>=0) leftHighUpf=upf[leftIndex].upf;
-	if(rightIndex>=0) rightHighUpf=upf[rightIndex].upf;
-	for(unsigned i=0; i<indices.size(); ++i){
-		float th=1.0f*upf[indices[i]].t;//t here
+	upf[index].upf/=2;//decrease upf near t by a factor
+	unsigned lowUpf=upf[index].upf;//get upf near t
+	unsigned leftHighUpf=1<<FRACTIONAL_BITS, rightHighUpf=1<<FRACTIONAL_BITS;//set upf near t-dt and t+dt to unity as a default value
+	if(leftIndex>=0) leftHighUpf=upf[leftIndex].upf;//if a left point was found, use it
+	if(rightIndex>=0) rightHighUpf=upf[rightIndex].upf;//if a right point was found, use it
+	for(unsigned i=0; i<indices.size(); ++i){//for all points in the dip
+		float th=1.0f*upf[indices[i]].t;//calculate t at this point (th="t here")
+		//interpolate to find the maximum allowable upf
 		unsigned maxUpf;
 		if(th>t)
 			maxUpf=(unsigned)linear(th, t, tf, 1.0f*lowUpf, 1.0f*rightHighUpf);
 		else
 			maxUpf=(unsigned)linear(th, ti, t, 1.0f*leftHighUpf, 1.0f*lowUpf);
+		//reduce upf here if needed
 		upf[indices[i]].upf=min(upf[indices[i]].upf, maxUpf);
 	}
 }
 
 void System::go(vector<float>* tooFasts){
-	//figure out a good timing
+	//start with u'(t)=1, or nearly so anyway -- the first upf point has to be (0, 0)
 	vector<UpfPoint> upf;
 	upf.push_back(UpfPoint(0, 0));
 	upf.push_back(UpfPoint(1<<FRACTIONAL_BITS, 1));
+	//figure out a good timing
 	while(true){
+		//run
 		computer.setTiming(upf);
 		simulate();
-		//perturb upf
+		//initialize perturbation stuff
 		bool good=true;
 		vector<float> coverage;
 		if(tooFasts) tooFasts->clear();
+		//motor constraints
 		for(unsigned i=0; i<computer.readHub().numberOfAxes(); ++i){
 			vector<unsigned> wentTooFast, acceleratedTooFast;
 			computer.readHub().readAxis(i).readMotor().readFasts(wentTooFast, acceleratedTooFast);
+			//perturb for motors that went too fast
 			for(int j=wentTooFast.size()-1; j>=0; --j){
 				float t=1.0f*wentTooFast[j]/computer.readHub().readAxis(i).readFrequencyMultiplier();
 				if(tooFasts) tooFasts->push_back(t);
 				dip(upf, t, coverage);
 				good=false;
 			}
+			//perturb for motors that accelerated too fast
 			for(int j=acceleratedTooFast.size()-1; j>=0; --j){
 				float t=1.0f*acceleratedTooFast[j]/computer.readHub().readAxis(i).readFrequencyMultiplier();
 				if(tooFasts) tooFasts->push_back(t);
@@ -435,18 +500,22 @@ void System::go(vector<float>* tooFasts){
 				good=false;
 			}
 		}
+		//get the position and max speed
 		const float samplePeriod=1.0f;//sample period chosen empirically
 		vector<vector<float> > position;
 		getPosition(position, samplePeriod);
 		vector<float> maxSpeed;
 		getMaxSpeed(maxSpeed, samplePeriod);
+		//figure out the speed based on the position and if it's above the max speed, perturb
 		for(unsigned i=position.size()-1; i>=1; --i){
+			//figure the speed
 			float speed=0.0f;
 			for(unsigned j=0; j<position[i].size(); ++j){
 				float d=position[i][j]-position[i-1][j];
 				speed+=d*d;
 			}
 			speed=sqrt(speed);
+			//perturb if it's above the max speed
 			if((i<maxSpeed.size()&&speed>maxSpeed[i])||speed>maxSpeed.back()){
 				float t=1.0f*i*samplePeriod;
 				if(tooFasts) tooFasts->push_back(t);
@@ -454,6 +523,7 @@ void System::go(vector<float>* tooFasts){
 				good=false;
 			}
 		}
+		//prepare for finishing, and finish if the run was successful
 		if(tooFasts) sort(tooFasts->begin(), tooFasts->end());
 		if(good) break;
 	}
@@ -464,7 +534,7 @@ void System::go(vector<float>* tooFasts){
 void System::getPosition(vector<vector<float> >& position, float samplePeriod) const{
 	unsigned sample=0, i=1;
 	while(true){
-		//go to the event after the current sample time
+		//go to the event after the current sample time, or finish if it's beyond the last event
 		float t;
 		if(samplePeriod<=0.0f) t=events[sample].t;
 		else t=sample*samplePeriod;
@@ -484,7 +554,7 @@ void System::getPosition(vector<vector<float> >& position, float samplePeriod) c
 void System::getU(vector<vector<float> >& u, float samplePeriod) const{
 	unsigned sample=0, i=1;
 	while(true){
-		//go to the event after the current sample time
+		//go to the event after the current sample time, or finish if it's beyond the last event
 		float t;
 		if(samplePeriod<=0.0f) t=events[sample].t;
 		else t=sample*samplePeriod;
@@ -504,14 +574,15 @@ void System::getU(vector<vector<float> >& u, float samplePeriod) const{
 void System::getUp(vector<float>& up, float samplePeriod) const{
 	unsigned sample=0, i=1;
 	while(true){
-		//go to the event after the current sample time
+		//go to the event after the current sample time, or finish if it's beyond the last event
 		float t=sample*samplePeriod;
 		while(i<computer.readUpf().size()&&computer.readUpf()[i].t<=t) ++i;
 		if(t>=events.back().t) break;
+		//if we're beyond the last upf point, use the last upf point
 		if(i==computer.readUpf().size())
 			up.push_back(1.0f*computer.readUpf().back().upf/(1<<FRACTIONAL_BITS));
+		//otherwise interpolate between previous event and this one
 		else{
-			//interpolate between previous event and this one
 			float s=(t-computer.readUpf()[i-1].t)/(computer.readUpf()[i].t-computer.readUpf()[i-1].t);
 			up.push_back(interpolate(1.0f*computer.readUpf()[i-1].upf, 1.0f*computer.readUpf()[i].upf, s)/(1<<FRACTIONAL_BITS));
 		}
@@ -521,41 +592,50 @@ void System::getUp(vector<float>& up, float samplePeriod) const{
 }
 
 void System::getError(vector<float>& error, float samplePeriod) const{
+	//get the position
 	vector<vector<float> > actualPosition;
 	getPosition(actualPosition, samplePeriod);
+	//get u
 	vector<vector<float> > u;
 	getU(u, samplePeriod);
+	//get the theoretical position with u, and compare it to the position
 	for(unsigned i=0; i<actualPosition.size(); ++i){
 		float e2=0.0f;
+		//pythagoras all the differences together
 		for(unsigned j=0; j<actualPosition[i].size(); ++j){
 			float theoreticalPosition;
 			computer.getTheoreticalPosition(u[i][j], j, theoreticalPosition);
-cerr<<actualPosition[i][j]<<" "<<theoreticalPosition<<"\n";///
 			float d=actualPosition[i][j]-theoreticalPosition;
 			e2+=d*d;
 		}
-cerr<<"\n";
+		//stick in the result
 		error.push_back(sqrt(e2));
 	}
 }
 
 float System::getMaxBandwidth(){
+	//start with 0 because that's the answer if there's nothing that ever happens
 	float m=0.0f;
+	//this is how many commands need to be sent throughout the entire run
 	unsigned initial=events[0].remainingMovements+events[0].remainingTimings;
+	//go through the run and keep track of max bandwidth
 	for(unsigned i=0; i<events.size(); ++i){
+		//divide the number of commands that were necessarily send by the current time
 		float b=(initial-(events[i].remainingMovements+events[i].remainingTimings))/events[i].t;
+		//record it if it's the biggest so far
 		m=max(m, b);
 	}
+	//finish
 	return m;
 }
 
 void System::getMaxSpeed(std::vector<float>& maxSpeed, float samplePeriod) const{
 	vector<float> up;
-	getUp(up, samplePeriod);
+	getUp(up, samplePeriod);//u' is used over u because the recorded u is different for each axis
 	float u=0.0f;
 	for(unsigned i=1; i<up.size(); ++i){
 		maxSpeed.push_back(computer.getMaxSpeed(u));
-		u+=(up[i]+up[i-1])/2*samplePeriod;
+		u+=(up[i]+up[i-1])/2*samplePeriod;//increase u by the average u' value over the sample period multiplied by the sample period
 	}
 }
 
--- daisy.hpp
+++ daisy.hpp
@@ -1,28 +1,99 @@
-/*
-The idea
+/** @mainpage
 Geometry is expressed to the system as a script. This can be interpreted as the
 parametric function x(u), where x is an n-vector, n being the number of axes.
-Now u doesn't have to be time. In fact, to avoid physical constraints, u will
-necessarily be different than time. But it itself is a function of time. So the
-idea is to start with u(t)=t and perturb it until all constraints are met.
+Now u doesn't have to be time. In fact, to avoid physical constraints, u may be
+different than time. But u is a function of time. So the idea is to start with
+u(t)=t and perturb it until all constraints are met.
+
+The system consists of a computer, a hub, axis controllers, and motors.
+- The computer talks to the hub.
+- The hub talks to the axis controllers.
+- Each axis controller talks to a motor.
+
+The actual physical system is open-loop. From start to end, the computer tells
+the motor where to go, but the motor never says back to anything "I got there."
+However, as long as certain physical constraints are met, motors should manage
+to get where they are told to go. Solving an optimal u as described above is a
+hard problem, so the system is simulated and perturbed to find a sufficient u.
+In this way feedback is possible, because a simulated motor can say "I might not
+have gotten there." So all in all, an open-loop is simulated and in some sense
+becomes closed-loop, but once the benefits of feedback are gained, the
+parameters are recorded and ready to be sent for an actual, final, physical,
+open-loop run.
+
+The specific perturbation done is not entirely important. Some solutions will
+work better than others.
 
 Some symbols
-up is the derivative of u. p is for prime.
-upf is the fractional part of up. f is for fractional.
-dupf is the change in upf.
+- up is the derivative of u. p is for prime.
+- upf is the fractional part of up. f is for fractional.
+- dupf is the change in upf. d is for delta.
 */
+
+/** @file */
 
 #include <string>
 #include <vector>
 #include <sstream>
 
+/** @var const unsigned FRACTIONAL_BITS
+Fixed point arithmetic is needed because some of this code is supposed to
+eventually run on microcontrollers without floating point arithmetic. Here, the
+number of bits which are fractional is defined. So, say there is a real number
+X which needs to be represented on a microcontroller. We define the integer
+variable xf. In pseudocode,
+
+X=xf/2^FRACTIONAL_BITS
+
+Naively, this translates to the following:
+
+X=xf>>FRACTIONAL_BITS
+
+However using this piece of code exactly would result in the fractional bits
+being truncated. But say we want to calculate X*Y with integer precision, where
+Y is an integer and so can be represented by the integer y in code. Then,
+
+X*Y=(xf/2^FRACTIONAL_BITS)*y
+
+X*Y=(xf>>FRACTIONAL_BITS)*y
+
+X*Y=(xf*y)>>FRACTIONAL_BITS
+
+And now the needed fractional bits come into play before the result is
+truncated.
+
+Unity in a fractional number is 1<<FRACTIONAL_BITS.
+
+To convert a fixed point number to a floating point number, we do
+
+floating=float(fixed)/(1<<FRACTIONAL_BITS)
+
+It might be helpful to think that dividing by unity here is like a unit
+conversion. Like there are 1000 m in a km, in some sense there are
+1<<FRACTIONAL_BITS fixed point numbers in a floating point number.
+*/
 const unsigned FRACTIONAL_BITS=8;
 
+/**
+When perturbing u, it is easiest to modify u'. When a constraint is
+violated at time t, u'(t) and its surroundings can be reduced. u'' must not
+have infinities; a piecewise linear u' is sufficient. It is also good to know
+that
+- 0<u' because the geometry should never be traced backward
+- u'<1 because u' is initialized to 1 for all t,
+	and the path should never be sped up in a perturbation.
+So u is stored as a bunch of u' points.
+*/
 struct UpfPoint{
 	UpfPoint(unsigned upf, unsigned t): upf(upf), t(t) {}
 	unsigned upf, t;
 };
 
+/**
+A representation of an actual physical motor. It doesn't have any smarts; the
+code here remains here. All it does is respond to ticks and steps and makes a
+note if it has gone too fast or accelerated too fast.
+*/
 class Motor{
 	public:
 		Motor(float maxSpeed, float maxAcceleration);
@@ -42,6 +113,39 @@ class Motor{
 		std::vector<unsigned> wentTooFast, acceleratedTooFast;
 };
 
+/**
+A representation of an axis controller, corresponding to a motor. Some code in
+here is representative of the code that would end up on an actual
+microcontroller, hence the fixed point variables.
+
+An axis controller handles two things: movements and timings. Movements are
+derived from the geometry defined by the user. They are the pieces of the
+stepwise function x(u). Timings are perturbed to fit the geometry and physical
+constraints outside the axis controller. They are the pieces of the linear
+stepwise function u'(t).
+
+Two similar points:
+- A motor will never step more than once per tick because this is physically
+	impossible.
+- The difference between u before and after a tick will never be more than 1,
+	because u'<1.
+
+As such, instead of doing something like
+
+y=f(x);
+
+we can do
+
+if(abs(y-f(x+1))<abs(y-f(x))) ++y;
+
+That is, check which of the closest two available slots is closest.
+If it is the next one, increment.
+
+There are three representations of u in this class. u, uT, and uM. u is only for
+simulating the system, and it's straightforward. uT and uM increment at the same
+time u does, but get reset to 0 when a new timing or movement happens. This
+makes calculations easier.
+*/
 class Axis{
 	public:
 		Axis(float maxSpeed, float maxAcceleration, unsigned frequencyMultipler);
@@ -72,6 +176,17 @@ class Axis{
 		int dupfSign;
 };
 
+/**
+A representation of a hub. Some code in here is representative of the code that
+would end up on an actual microcontroller. The hub is responsible for receiving
+instructions from a computer and then translating them to individual axis
+instructions.
+
+The hub is mostly complicated here because it has to allow the movement of the
+system to be documented so it can be checked and perturbed if needed. The hub is
+oversimple because it doesn't really deal with bandwidth right now, instead just
+sending all information to the axes up front.
+*/
 class Hub{
 	public:
 		Hub();
@@ -89,6 +204,11 @@ class Hub{
 		unsigned subTicks, subTicksPerTick;
 };
 
+/**
+A representation of the computer. Don't be scared by the circularity here. In an
+actual physical system, some of this code will be used to interpret more-human
+instructions and translate and send them to the hub.
+*/
 class Computer{
 	public:
 		std::string setScript(std::string);
@@ -115,16 +235,62 @@ class Computer{
 		std::vector<UpfPoint> upf;
 };
 
+/**
+A representation of the entire physical system, including computer, hub, axes,
+and motors. None of this code is used during the final physical run. It is used
+beforehand to predict how the physical system will respond and allows a
+successful timing to be perturbed.
+*/
 class System{
 	public:
+		/**
+		Supply the more-human description of the geometry.
+		Call this before calling go.
+		All time is measured in ticks and time measured in steps. So a speed is in ticks per step.
+		A script goes like this. First, state your axes. This line
+		axis <maxSpeed> <maxAcceleration> <frequencyMultiplier>
+		declares a single axis; you'll have as many of these lines as you do axes.
+		frequencyMultiplier is how much faster this axis's clock goes compared to the hub clock.
+		You can specify a maximum actual speed like so:
+		maxSpeed <maxSpeed>
+		You can do this any time after axes have been declared. It will apply to all following lines.
+		To specify a line,
+		line <dx1> <dx2> <dx3> ... <dxn>
+		Where n is the number of axes, and dxi is the amount axis i should move.
+		*/
 		std::string setScript(std::string);
+
+		/**
+		Find a working timing function for the previously set script,
+		run with the final timing,
+		and document that run so that the "get" functions can be called.
+		*/
 		void go(std::vector<float>* tooFasts=NULL);
+
+		/**
+		Get position as a function of time.
+		Position is a vector of samples in time, each sample is a vector of motor positions.
+		If samplePeriod is 0, each sample is given instead of interpolated to ensure a consistent time between samples.
+		*/
 		void getPosition(std::vector<std::vector<float> >& position, float samplePeriod=0.0f) const;
+
+		/** get u(t) */
 		void getU(std::vector<std::vector<float> >& u, float samplePeriod=0.0f) const;
+
+		/** get u'(t) */
 		void getUp(std::vector<float>& up, float samplePeriod) const;
+
+		/** Get error as a function of time, axes all pythagoreaned together. */
 		void getError(std::vector<float>& error, float samplePeriod) const;
+
+		/**
+		Get the minimum max bandwidth required to complete the run.
+		That is, the max bandwidth required could be infinity, if everything were sent up front at once, but this is a dumb strategy.
+		Even though all commands are sent up front in the simulation,
+		we can still know the maximum number required per time interval.
+		Infinite memory is assumed, but reducing bandwidth tends to reduce required memory.
+		*/
 		float getMaxBandwidth();
-		void getMaxSpeed(std::vector<float>& maxSpeed, float samplePeriod) const;
 	private:
 		struct Event{
 			float t;
@@ -132,6 +298,7 @@ class System{
 			unsigned remainingMovements, remainingTimings;
 			std::vector<unsigned> u;
 		};
+		void getMaxSpeed(std::vector<float>& maxSpeed, float samplePeriod) const;
 		void simulate();
 		void sample(float t);
 		Computer computer;
